7811b59655fbc9d62a266cd3e22a8b2b166c70bc
alain17-web <web2020.alain@gmail.com>
2021-10-07 19:55:52 +0200

    main

--- woodstock/src/App.js
+++ woodstock/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component} from 'react'
 import './App.css'
-import './components/Navbar'
+//import './components/Navbar'
 import Navbar from './components/Navbar'
 import Top from './components/Top'
+import Main from './components/Main'
+import Team from './components/Team'
 
 class App extends Component {
 
@@ -14,6 +16,8 @@ class App extends Component {
       <div className="container">
         <Navbar/>
         <Top/>
+        <Main/>
+        <Team/>
       </div>
     )
   }
---
+++ woodstock/src/components/Main.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './Main.css'
+import about from '../img/about.png'
+import shop from '../img/shop.png'
+
+
+const Main = () => {
+    return(
+        <div className="container pb-5 mb-5">
+            <div className="row gx-5">
+                <div className="col-sm-6">
+                    <img src={about} className="img-fluid" alt="about" />
+                </div>
+                <div className="col-sm-6 mt-5 mb-5">
+                    <h3 className="fs-5"><span id="about">|</span> ABOUT US</h3>
+                    <p className="pt-3">Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin'house, fuckin'up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'coz I'll kill the motherfucker. know what I'm sayin' ?</p>
+                    <button className="mt-5 bg-light py-2 px-3">LEARN MORE</button>
+                </div>
+            </div>
+            <div className="row mt-5 gx-5 mb-5 pb-5">
+                <div className="col-sm-6 mt-5">
+                    <h3 className="fs-5"><span id="shop">|</span> SHOP</h3>
+                    <p className="pt-3">Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin'house, fuckin'up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'coz I'll kill the motherfucker. know what I'm sayin' ?</p>
+                    <button className="mt-5 bg-light py-2 px-3">Go Shopping</button>
+                </div>
+                <div className="col-sm-6">
+                    <img src={shop} className="img-fluid" alt="shop"/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main
